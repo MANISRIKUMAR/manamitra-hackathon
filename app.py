1dfbf25b175ba89bc17e293ef82bb28eaf82ab65
JALLELLA MANI SRI KUMAR <jallellamanisrikumar@gmail.com>
2025-06-12 02:29:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,6 @@ def respond():
     return jsonify({"reply": reply})
 
 if __name__ == '__main__':
-    if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
